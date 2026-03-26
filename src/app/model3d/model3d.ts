630dff89a38365a3247d05e4bb6ca08fd6dc8c48
lopez <14julianlopez@gmail.com>
2026-03-25 20:31:18 -0500

    select block and change color

--- src/app/model3d/model3d.ts
+++ src/app/model3d/model3d.ts
@@ -1,6 +1,8 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, NgZone, ViewChild, signal } from '@angular/core';
 import * as THREE from "three";
 import { OrbitControls }  from "three/examples/jsm/controls/OrbitControls.js";
+import { TEXTURE_MAP } from '../constants/textures/textures.constant';
+
 
 @Component({
   selector: 'app-model3d',
@@ -9,15 +11,35 @@ import { OrbitControls }  from "three/examples/jsm/controls/OrbitControls.js";
   styleUrl: './model3d.css',
 })
 export class Model3d implements AfterViewInit {
+
+
+  //diccionario de texturas
+  public textures = TEXTURE_MAP;
+
+  // posicion boton agregar cubo
+  buttonX = 0;
+  buttonY = 0;
+
+  // Div donde se creará el render de la vision 3D
   @ViewChild('modelado', { static: false })
   private container!: ElementRef;
+  // Variables necesarias para THREE
+
   private scene!: THREE.Scene;
   private camera!: THREE.PerspectiveCamera;
   private renderer!: THREE.WebGLRenderer;
   private textureLoader!: THREE.TextureLoader;
-  private controls!: OrbitControls;
+  private controls!: OrbitControls; // Movimiento del mouse
+  private raycaster!: THREE.Raycaster; // Necesario para saber que objeto se selecciona
+  private mouse!: THREE.Vector2;
 
+  // Logica de cubo seleccionado
+  selectedCube: THREE.Mesh | null = null;
 
+  selectCube(cube: THREE.Mesh): void
+  { if (this.selectedCube)
+  { (this.selectedCube.material as THREE.MeshStandardMaterial) .color.set(0xaaaaaa); }
+    this.selectedCube = cube; (cube.material as THREE.MeshStandardMaterial) .color.set(0xff0000); }
 
   ngAfterViewInit(): void {
     this.initThree();
@@ -25,100 +47,155 @@ export class Model3d implements AfterViewInit {
   }
 
   initThree(){
-    const fov = 75;
-    const aspectRatio = window.innerWidth / window.innerHeight;
-    const near = 0.1;
-    const far = 1000;
 
-    this.scene = new THREE.Scene();
-
+    //Parte de la camara
+    const fov: number = 75;
+    const aspectRatio : number = window.innerWidth / window.innerHeight;
+    const near: number = 0.1;
+    const far: number = 1000;
     this.camera = new THREE.PerspectiveCamera(fov, aspectRatio, near, far);
     this.camera.position.z = 5;
 
+    //----------------------------
+
+    //Parte de la renderizacion
+      //tamanio del render
+    const widthRenderer: number = 1200;
+    const heightRenderer: number = 700;
     this.renderer = new THREE.WebGLRenderer({antialias: true});
-    this.renderer.setSize(1500, 1500);
+    this.renderer.setSize(1200, 500); //
     this.container.nativeElement.appendChild(this.renderer.domElement);
 
+    //----------------------------
+
+    // Parte de la escena
+    this.scene = new THREE.Scene();
+      // iluminacion
+    const intensityLigth: number = 3;
+    const ambientLight = new THREE.AmbientLight(0xffffff, intensityLigth);
+    this.scene.add(ambientLight);
+
+    //-----------------------------
+
+    // Parte de controles y movimiento
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+
+    //-----------------------------
+
+    // Guias
+      // Desde centro X, Y, Z
+    const axesHelper = new THREE.AxesHelper(10);
+    this.scene.add(axesHelper);
+      // malla
+    const gridHelper = new THREE.GridHelper(200, 150, 0xffffff, 0xffffff)
+    this.scene.add(gridHelper);
+
+    // cargue de texturas iniciales
     this.textureLoader = new THREE.TextureLoader();
-    const grassTexture = this.textureLoader.load("textures/grass.jpg");
-    const concreteTexture = this.textureLoader.load("textures/concrete.jpg");
 
-    concreteTexture.wrapS = THREE.RepeatWrapping;
-    concreteTexture.wrapT = THREE.RepeatWrapping;
+    // cesped
+      this.initGrass();
 
-    concreteTexture.repeat.set(1, 1);
+    //--------------------------------
 
-    const geometry = new THREE.BoxGeometry(1, 1, 5);
+    // construir cubo inicial
+    this.buildCube(0,0);
 
-    const material = new THREE.MeshStandardMaterial({
-      map: concreteTexture
-    });
-    const cube = new THREE.Mesh(geometry, material);
+    // necesario para seleccion cubo mouse
+    this.mouse = new THREE.Vector2();
+    this.raycaster = new THREE.Raycaster();
 
-    cube.position.set(0, 0.5, 0);
 
-    this.scene.add(cube);
+    this.renderer.domElement.addEventListener('click', (event) => {
 
+      const rect = this.renderer.domElement.getBoundingClientRect();
+
+      this.mouse.x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
+      this.mouse.y = -((event.clientY - rect.top) / rect.height) * 2 + 1;
+
+      this.raycaster.setFromCamera(this.mouse, this.camera);
+      const intersects = this.raycaster.intersectObjects(this.scene.children);
+
+      const cubeHit = intersects.find(obj =>
+        obj.object instanceof THREE.Mesh &&
+        obj.object.geometry instanceof THREE.BoxGeometry
+      );
+
+      if (cubeHit) { this.selectCube(cubeHit.object as THREE.Mesh); }
+
+    })
+
+  }
+
+  initGrass(){
+    const grassTexture = this.textureLoader.load(this.textures.grass.default);
     grassTexture.wrapS = THREE.RepeatWrapping;
     grassTexture.wrapT = THREE.RepeatWrapping;
-
     grassTexture.repeat.set(20, 20);
-
     grassTexture.anisotropy = this.renderer.capabilities.getMaxAnisotropy();
-
-    const axesHelper = new THREE.AxesHelper(5);
-    this.scene.add(axesHelper);
-
-
     const groundGeometry = new THREE.PlaneGeometry(100, 100);
-
     const groundMaterial = new THREE.MeshStandardMaterial({
       map: grassTexture
     });
-
     const ground = new THREE.Mesh(groundGeometry, groundMaterial);
     ground.rotation.x = -Math.PI / 2;
-
-    this.scene.add(ground);
-
     grassTexture.colorSpace = THREE.SRGBColorSpace;
+    this.scene.add(ground);
+  }
 
-    const gridHelper = new THREE.GridHelper(200, 150, 0xff0000, 0x444444)
-    this.scene.add(gridHelper);
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+  initConcrete() :THREE.MeshStandardMaterial{
+    const concreteTexture = this.textureLoader.load(this.textures.concrete.default);
+    concreteTexture.wrapS = THREE.RepeatWrapping;
+    concreteTexture.wrapT = THREE.RepeatWrapping;
+    concreteTexture.repeat.set(1, 1);
+    const material = new THREE.MeshStandardMaterial({
+      map: concreteTexture
+    });
+    return material;
+  }
 
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
-    this.scene.add(ambientLight);
+  buildCube(x:number,z:number){
+    const geometry = new THREE.BoxGeometry(1, 1, 5);
+    const cube = new THREE.Mesh(geometry, this.initConcrete());
+    cube.position.set(x, 0.5, z);
+    this.scene.add(cube);
+  }
 
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-    directionalLight.position.set(10, 10, 10);
-    this.scene.add(directionalLight);
+  crearBloqueDesdeSeleccion() {
+    if (!this.selectedCube) return;
 
-    const boton = document.getElementById("crearBloque");
+    const geometry = new THREE.BoxGeometry(1,1,1);
+    const material = new THREE.MeshStandardMaterial({ color: 0xaaaaaa });
 
-    boton?.addEventListener("click", () => {
+    const newCube = new THREE.Mesh(geometry, material);
 
-      let x = document.getElementById("xblock") as HTMLInputElement;
-      let z = document.getElementById("zblock") as HTMLInputElement;
+    newCube.position.set(
+      this.selectedCube.position.x + 1,
+      this.selectedCube.position.y,
+      this.selectedCube.position.z
+    );
 
-      let xnumber = Number(x.value);
-      let znumber = Number(z.value);
+    this.scene.add(newCube);
+  }
 
-      const geometry = new THREE.BoxGeometry(1,1,5);
-      const material = new THREE.MeshStandardMaterial({color: 0xaaaaaa});
+  updateButtonPosition() {
+    if (!this.selectedCube) return;
 
-      const cube = new THREE.Mesh(geometry, material);
+    const vector = this.selectedCube.position.clone();
+    vector.y += 1;
 
-      cube.position.set(xnumber,0.5,znumber);
+    vector.project(this.camera);
 
-      this.scene.add(cube);
+    const rect = this.container.nativeElement.getBoundingClientRect();
 
-    } )
+    this.buttonX = (vector.x * 0.5 + 0.5) * rect.width;
+    this.buttonY = (-vector.y * 0.5 + 0.5) * rect.height;
   }
 
   animate = () => {
     requestAnimationFrame(this.animate);
     this.controls.update();
     this.renderer.render(this.scene,this.camera);
+    this.updateButtonPosition();
   }
 }
